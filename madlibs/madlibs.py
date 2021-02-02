32730bfa584f4760a96c2159ffa6d3dec115a859
TundeOlagunju <tundeolagunju99@gmail.com>
2021-02-02 07:11:25 +0000

    Extracted method

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -6,8 +6,11 @@ madlibsFile.close()
 upperCaseWords  = re.findall('[A-Z][A-Z]+', madlibsContent)
 vowels = ('a','e','i','o','u')
 
+def construct_question(word, determiner):
+    return f'Enter {determiner} {word.lower()} \n'
+
 for word in upperCaseWords:
-    replace = input(f'Enter an {word.lower()} \n') if word.lower().startswith(vowels) else input(f'Enter a {word.lower()} \n')
+    replace = input(construct_question(word, 'an')) if word.lower().startswith(vowels) else input(construct_question(word, 'a'))
     madlibsContent = madlibsContent.replace(word, replace)
 
 madlibsFile = open('madlibs.txt', 'w')
