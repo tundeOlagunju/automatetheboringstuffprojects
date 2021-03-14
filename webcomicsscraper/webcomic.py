c6a211963183b73f28f6222a6c9157ac39f4d36f
TundeOlagunju <tundeolagunju99@gmail.com>
2021-03-14 04:49:01 +0000

    complete extraction of title and image tags

--- webcomicsscraper/webcomic.py
+++ webcomicsscraper/webcomic.py
@@ -2,11 +2,12 @@ import jsonpickle
 from random import random
 import network
 import logging
+from enum import Enum
 
 
-class WebComicDownloadState(object):
+class WebComicDownloadStatus(Enum):
     NOT_STARTED = 0
-    FAILED_RESPONSE = 1
+    FAILED = 1
     SUCCESS = 2
 
 
@@ -14,19 +15,19 @@ class WebComicDownloadState(object):
 class WebComicDownloaddata(object):
     def __init__(self):
         self.url = ''
-        self._last_downloaded_image_link = ''
+        self._latest_img_url = ''
         self.last_tried = ''
-        self.last_download_state = WebComicDownloadState.NOT_STARTED
+        self.last_download_status = WebComicDownloadStatus.NOT_STARTED.name
         self.cache = ''
     
     @property
-    def last_downloaded_image_link(self):
-        return self._last_downloaded_image_link
+    def latest_img_url(self):
+        return self._latest_img_url
     
-    @last_downloaded_image_link.setter  
-    def last(self, current_img_link):
-        if current_img_link and current_img_link != self._last_downloaded_image_link : 
-            self._last  = current_img_link
+    @latest_img_url.setter  
+    def latest_img_url(self, curr_img_url):
+        if curr_img_url and curr_img_url != self._latest_img_url: 
+            self._latest_img_url  = curr_img_url
 
 
 
@@ -35,10 +36,10 @@ class WebComic(object):
     def __init__(self, download_data):
         self._comic_html = ''
         self._extractor = None
-        self.img_links = []
+        self.img_urls = []
         self.title = ''
         self.download_data = download_data
-        self.download_data.last_download_state = WebComicDownloadState.NOT_STARTED
+        self.download_data.last_download_status = WebComicDownloadStatus.NOT_STARTED.name
 
     @property
     def extractor(self):
@@ -58,9 +59,9 @@ class WebComic(object):
     def comic_html(self, comic_html):
         if comic_html:
             self._comic_html = comic_html
-            self.download_data.last_download_state = WebComicDownloadState.SUCCESS
+            self.download_data.last_download_status = WebComicDownloadStatus.SUCCESS.name
         else: 
-            self.download_data.last_download_state = WebComicDownloadState.FAILED_RESPONSE
+            self.download_data.last_download_status = WebComicDownloadStatus.FAILED.name
             logging.debug('Html content is empty')
 
     def download(self):
@@ -69,15 +70,15 @@ class WebComic(object):
             html = network.download_html(self.download_data.url)
             self.comic_html = html
         except Exception as e:
-            self.download_data.last_download_state = WebComicDownloadState.FAILED_RESPONSE
+            self.download_data.last_download_status = WebComicDownloadStatus.FAILED.name
             logging.debug('Download failed on URL, %s because of %s', self.download_data.url, str(e) )
 
-    def extract_img_links(self):
-        img_links = self.extractor.get_img_links()
-        self.img_links = img_links
+    def fetch_img_urls(self):
+        img_urls = self.extractor.extract_img_urls()
+        self.img_urls = img_urls
        
-    def extract_title(self):
-        title = self.extractor.get_title()
+    def fetch_title(self):
+        title = self.extractor.extract_title()
         self.title = title
 
     def __repr__ (self):
--- webcomicsscraper/webscraper.py
+++ webcomicsscraper/webscraper.py
@@ -1,9 +1,10 @@
 from contentextractor import ContentExtractor
 from fileutils import FileUtils
-from webcomic import WebComicDownloadState, WebComicDownloaddata, WebComic
+from webcomic import WebComicDownloadStatus, WebComicDownloaddata, WebComic
 
 import logging
 import jsonpickle
+import sys
 
 class WebComicScraper(object):
 
@@ -17,19 +18,19 @@ class WebComicScraper(object):
         logging.info('Reading and deserializing input json %s', self.file_path)
         input_json = FileUtils.read_json_file(self.file_path)
         comic_download_data = FileUtils.decode_json(input_json)
-        logging.info('Input json deserialized successfully. List of webcomics %s', [data.url for data in comic_download_data])
+        logging.info('Input json contains List of webcomics %s', [data.url for data in comic_download_data])
         
 
         for i, comicdata in enumerate(comic_download_data):
             webcomic = WebComic(comicdata)
-            if comicdata == '' or comicdata == None:
-                comicdata.last_download_state = WebComicDownloadState.FAILED_RESPONSE
+            if not comicdata:
+                comicdata.last_download_status = WebComicDownloadStatus.FAILED.name
                 logging.info('Webcomic at position %s does not contain a url. Skipping..', i+1)
                 continue
 
             logging.info('Downloading page %s ....', comicdata.url)
             webcomic.download()
-            if webcomic.download_state != WebComicDownloadState.SUCCESS:
+            if webcomic.download_data.last_download_status != WebComicDownloadStatus.SUCCESS.name:
                 logging.info('Skipping %s as page download was not successful', comicdata.url)
                 continue
             logging.info('Page download successful for %s', comicdata.url)
@@ -37,12 +38,17 @@ class WebComicScraper(object):
             content_extractor = ContentExtractor(webcomic.comic_html)
             webcomic.extractor = content_extractor
 
-            webcomic.extract_img_links()
+            logging.info('Extracting all image links in %s ....', comicdata.url)
+            webcomic.fetch_img_urls()
+            logging.info('Finished extracting all image links in %s successfully', comicdata.url)
+            
+            logging.info('Fetching the title in %s ....', comicdata.url)
+            webcomic.fetch_title()
 
-        import sys
-        print(sys.getrecursionlimit())
-        sys.setrecursionlimit(15000)
-        # print(webcomics)
+            # Theres still an edge case with title
+            print(webcomic.title)
+        
+
         print(jsonpickle.encode(comic_download_data, unpicklable=False))
         
 
