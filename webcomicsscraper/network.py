a9b0840880151fdf6aeceb72b9c10ca298ad5c78
TundeOlagunju <tundeolagunju99@gmail.com>
2021-03-20 09:24:18 +0000

    make download_image conform to SRP

--- webcomicsscraper/network.py
+++ webcomicsscraper/network.py
@@ -1,4 +1,5 @@
-import requests,bs4
+import requests,bs4, logging
+import files, urls
 
 def download_html(url):
     res = requests.get(url)
@@ -6,4 +7,10 @@ def download_html(url):
     res.raise_for_status()
     return html or ''
     
-    
+
+def download_image(img_url):
+    prep_url = urls.prepare_img_url(img_url)
+    print("prep_url", prep_url)
+    res = requests.get(prep_url)
+    res.raise_for_status()
+    return res.content
