2120c463d3eb6146019445abb0740f241ae22bef
TundeOlagunju <tundeolagunju99@gmail.com>
2021-03-21 09:02:52 +0000

    clean image url before downloading

--- webcomicsscraper/network.py
+++ webcomicsscraper/network.py
@@ -1,16 +1,26 @@
 import requests,bs4, logging
-import files, urls
+import urls
 
 def download_html(url):
     res = requests.get(url)
     html = bs4.BeautifulSoup(res.text, 'html.parser')
     res.raise_for_status()
-    return html or ''
+    return html
     
 
-def download_image(img_url):
-    prep_url = urls.prepare_img_url(img_url)
-    print("prep_url", prep_url)
-    res = requests.get(prep_url)
+def download_image(img_url, url):
+    """Tries to download the image with the given img url, if that fails, the image url is then refined further"""
+
+    try_raw_url = None
+    try:
+        res = requests.get(img_url)
+        try_raw_url = res.status_code == 200
+    except Exception as e:
+        logging.debug('Exception occured while trying to download image because of %s', e)
+
+    if not try_raw_url:
+        prep_url = urls.prepare_img_url(img_url, url)
+        res = requests.get(prep_url)
+
     res.raise_for_status()
     return res.content
--- webcomicsscraper/urls.py
+++ webcomicsscraper/urls.py
@@ -1,25 +1,60 @@
-import tldextract, os, files
+import tldextract, os, files, re
 
 #parse domain manually 
 # prep img url
 
 def get_domain_from_url(url):
-    ext = tldextract.extract(url)
+    if not url:
+        return None
+    ext = tldextract.extract(url.strip())
     return ext.domain
 
-def prepare_img_url(url):
-    if url is None:
+def remove_leading_slashes(url):
+    if not url:
         return None
-    if url.startswith(('http', 'https')):
-        return url
-    return 'http:' + url
-    
+    return url.strip('//')
 
+def remove_trailing_slashes(url):
+    if not url:
+        return None
+    if url.endswith('/'):
+        url = url[:-1]
+    return url
+
+def prepare_img_url(img_url, url):
+    if not url:
+        return None
+    img_url = remove_leading_slashes(img_url)
+    url = remove_trailing_slashes(url)
+    domain = get_domain_from_url(url)
+    if domain in img_url:
+        return 'http://' + img_url
+    return url + '/' + img_url
+
+def clean_img_file_name(file_name):
+    """
+    Cleans image file name. Turns gdgdggdgd.png?sgsgsgjdueydnnd into gdgdggdgd.png. Does nothing 
+    if the image file name is already clean
+    """
+
+    #get the file extension and remove all characters after that
+    ext_regex = re.compile(r'(jpg|png|gif)')
+    ext = ext_regex.search(file_name).group()
+    cleaned_name = file_name.split(ext)[0] + ext
+    return cleaned_name
+
+
+def get_file_name (img_url):
+    # last part of the image url
+    base_name = os.path.basename(img_url)
+    file_name = clean_img_file_name(base_name)
+    return file_name
+    
 def get_img_file_path(url, img_url):
     domain = get_domain_from_url(url)
     img_dir = files.join_path('bin', domain)
     files.make_dir(img_dir)
-    # last part of the image url
-    img_name = os.path.basename(img_url)
-    img_file_path = files.join_path(img_dir, img_name)
-    return img_file_path
+
+    file_name = get_file_name(img_url)
+    file_path = files.join_path(img_dir, file_name)
+    return file_path
